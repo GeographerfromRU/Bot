310332a961becc993f2317cb56aa0da69def3e99
Vitaliy P <renamee@Yandex.ru>
2021-08-01 14:06:38 +0300

    third

--- bot.py
+++ bot.py
@@ -11,22 +11,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '1906828102:AAFpGRVV5t27ywJnV8C4oELGrf37qRo8nRI'
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 def echo(update, context):
-    """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
@@ -50,10 +44,10 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook (listen = "0.0.0.0", 
-                          port = int (PORT), 
-                          url_path = TOKEN) 
-    updater.bot.setWebhook ('https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
+    updater.start_webhook(listen = "0.0.0.0", 
+                          port=PORT, 
+                          url_path=TOKEN 
+                          webhook_url='https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
