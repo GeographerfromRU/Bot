f3993fda1ee40a254553a64fe604551790b5abf5
Vitaliy P <renamee@Yandex.ru>
2021-08-01 17:23:53 +0300

    32

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def echo(update, context):
     if "Сергей" in user_says:
         update.message.reply_text("@MarkSulla, ты работу нашел?")
     elif "Эльдар" in user_says:
-        update.message.text("привет бала")
+        update.message.reply_text("привет бала")
     elif "усы" in user_says:
         update.message.reply_text("@MarkSulla усы побрил?")
     elif "Бала" in user_says:
