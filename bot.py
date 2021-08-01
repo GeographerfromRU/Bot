9d9dfcdf8d54de012cab6b7b0cbb31ddd9cbfe49
Vitaliy P <renamee@Yandex.ru>
2021-08-01 14:26:01 +0300

    seven

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ def main():
     # Start the Bot
     updater.start_webhook(listen = "0.0.0.0", 
                           port=int(PORT),
-                          url_path=TOKEN 
+                          url_path=TOKEN,
                           webhook_url='https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
