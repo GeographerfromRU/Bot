c079b3f96fa6df09d88a57d7bffdf97c3350677f
Vitaliy P <renamee@Yandex.ru>
2021-08-01 17:15:57 +0300

    twenty_eight

--- bot.py
+++ bot.py
@@ -21,9 +21,9 @@ def help(update, context):
 def echo(update, context):
     user_says = update.message.text
     if "Сергей" in user_says:
-        update.message.text("@MarkSulla, ты работу нашел?")
+        update.message.reply_text("@MarkSulla, ты работу нашел?")
     elif "Эльдар" in user_says:
-        update.message.text("привет бала")
+        update.message.reply_text("привет бала")
     elif "усы" in user_says:
         update.message.reply_text("@MarkSulla усы побрил?")
     elif "Бала" in user_says:
