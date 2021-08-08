ef53a0a579379fecd7527f9ce5e3189a4dc200b2
Vitaliy P <renamee@Yandex.ru>
2021-08-08 20:28:26 +0300

    webhook

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
+                          port=PORT,
                           url_path=TOKEN)
     updater.bot.setWebhook('https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
     # Start the Bot
