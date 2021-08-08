23595339959b7efbf3a341f6936c05648ea04a68
Vitaliy P <renamee@Yandex.ru>
2021-08-08 20:24:45 +0300

    webhook

--- bot.py
+++ bot.py
@@ -87,7 +87,10 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
     # Start the Bot
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
