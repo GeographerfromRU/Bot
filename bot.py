6d6d0736f848cd5ae942cf194202e0cdbde7e03a
mmushko <mmushko@cisco.com>
2021-08-09 17:30:16 +0200

    return heroku webhook

--- bot.py
+++ bot.py
@@ -87,11 +87,11 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 
-    updater.start_polling()
-    #updater.start_webhook(listen="0.0.0.0",
-     #                     port=int(PORT),
-     #                     url_path=TOKEN,
-      #                    webhook_url='https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
+    #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN,
+                          webhook_url='https://stormy-thicket-52208.herokuapp.com/' + TOKEN)
     # Start the Bot
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
